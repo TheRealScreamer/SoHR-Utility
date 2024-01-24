3603838460a5770af61c783400ba5990ce520fd8
Albert <Soundofhoperecordings.albert@gmail.com>
2024-01-23 22:04:23 -0700

    Cosmetic changes, dsp coding

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -18,14 +18,34 @@ SoHRUtilityAudioProcessorEditor::SoHRUtilityAudioProcessorEditor (SoHRUtilityAud
     trimKnob.setRange(-96.0, 48.0, 0.25); /*sets the min and max of the knob (Min, Max, Interval Value)*/
     trimKnob.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 66, 28); /*changes position and size of text box    (sets TB below Knob, Type in Box?, Width, Height)*/
     trimKnob.setDoubleClickReturnValue(true, 0.0); /*sets knob back to 0 when double clicked*/
+    trimKnob.setColour(juce::Slider::ColourIds::thumbColourId, juce::Colours::purple.withAlpha(0.0f)); /*hides the thumb of the knob*/
+    trimKnob.setColour(juce::Slider::ColourIds::rotarySliderFillColourId, juce::Colours::purple.withAlpha(1.0f)); /*color of slider*/
+    trimKnob.setColour(juce::Slider::ColourIds::rotarySliderOutlineColourId, juce::Colours::ghostwhite.withAlpha(0.25f)); /*color of slider background*/
+    trimKnob.setColour(juce::Slider::ColourIds::textBoxTextColourId, juce::Colours::purple.withAlpha(1.0f)); /*color of values under knob*/
+    trimKnob.setColour(juce::Slider::ColourIds::textBoxOutlineColourId, juce::Colours::purple.withAlpha(0.0f)); /*color of the box around text above*/
+
+    trimKnob.onValueChange = [this]()
+        {
+            audioProcessor.volume = trimKnob.getValue();
+        };
     
     addAndMakeVisible(phaseInvertBut); /*Makes the Phase Button Visible*/
     phaseInvertBut.setClickingTogglesState(true); /*makes the button clickable*/
+    phaseInvertBut.setColour(juce::TextButton::ColourIds::buttonColourId, juce::Colours::white.withAlpha(0.5f)); /*colors the button when off*/
+    phaseInvertBut.setColour(juce::TextButton::ColourIds::buttonOnColourId, juce::Colours::ghostwhite.withAlpha(0.25f)); /*colors the button when on*/
+    phaseInvertBut.setColour(juce::TextButton::ColourIds::textColourOnId, juce::Colours::purple.withAlpha(0.5f)); /*button text on color*/
+    phaseInvertBut.setColour(juce::TextButton::ColourIds::textColourOffId, juce::Colours::purple.withAlpha(1.0f)); /*button text off color*/
+
+    phaseInvertBut.onClick = [this]()
+        {
+            audioProcessor.phaseState = phaseInvertBut.getToggleState();
+        };
 
     addAndMakeVisible(trimKnobLabel); /*Makes the Trim Knob Label Visible*/
     trimKnobLabel.setText("Trim", juce::dontSendNotification); /*Generates the text above the knob*/
     trimKnobLabel.setJustificationType(juce::Justification::centred); /*Makes the text centered*/
     trimKnobLabel.attachToComponent(&trimKnob, false); /*Attaches the text to the knob*/
+    trimKnobLabel.setColour(juce::Label::ColourIds::textColourId, juce::Colours::purple.withAlpha(1.0f)); /*sets text color*/
 
     setSize (400, 400);/*pixel size of plugin*/
 }
@@ -38,7 +58,7 @@ SoHRUtilityAudioProcessorEditor::~SoHRUtilityAudioProcessorEditor()
 void SoHRUtilityAudioProcessorEditor::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+    g.fillAll (juce::Colours::black);
 }
 
 void SoHRUtilityAudioProcessorEditor::resized()
