015c6ace9ac1c905dadb9d0b271d896d6f5dcc60
Albert <Soundofhoperecordings.albert@gmail.com>
2024-01-23 19:39:14 -0700

    Created Trim Knob, Removed Comments, Made Knob Visible, Resized Plugin Window, Changed Displayed text, Removed Comments, Set the location of the Knob

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -13,9 +13,8 @@
 SoHRUtilityAudioProcessorEditor::SoHRUtilityAudioProcessorEditor (SoHRUtilityAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    addAndMakeVisible(trimKnob);
+    setSize (400, 400);
 }
 
 SoHRUtilityAudioProcessorEditor::~SoHRUtilityAudioProcessorEditor()
@@ -30,11 +29,10 @@ void SoHRUtilityAudioProcessorEditor::paint (juce::Graphics& g)
 
     g.setColour (juce::Colours::white);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.drawFittedText ("Sound of Hope Recordings", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void SoHRUtilityAudioProcessorEditor::resized()
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
+    trimKnob.setBounds(getLocalBounds());
 }
